ea1cd1a7f22401a44585eb469da9b148e59dc010
Glagan <Glagan@users.noreply.github.com>
2022-09-24 11:48:31 +0200

    Add database

---
+++ database/init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE transactions (
+    id BIGSERIAL PRIMARY KEY,
+    market VARCHAR (36) NOT NULL,
+    broker VARCHAR (36) NOT NULL,
+    broker_order_id VARCHAR (36) NOT NULL,
+    instrument VARCHAR (36) NOT NULL,
+    quantity INTEGER NOT NULL,
+    price DECIMAL NOT NULL,
+    status VARCHAR(10) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
